f3fe2966e24a9ae55c9ce1d82afdc9e10b7f191f
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2023-08-17 22:05:46 -0300

    [UPDATE] Classe de DespesaTest: DisplayName para melhor visualização dos testes

--- src/test/DespesaTest.java
+++ src/test/DespesaTest.java
@@ -4,12 +4,14 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import exception.TextoNuloException;
 import exception.ValorDespesaException;
 import model.Despesa;
 
+@DisplayName("Classe de teste: Despesa")
 class DespesaTest {
 	
 	Despesa despesa;
@@ -20,11 +22,13 @@ class DespesaTest {
 	}
 	
 	@Test
+	@DisplayName("Exception: campo loja vazio")
 	void testSetLojaVazio() {
 		assertThrows(TextoNuloException.class, () -> despesa.setLoja(""));
 	}
 
 	@Test
+	@DisplayName("Setando Loja para String de apenas uma letra")
 	void testSetLojaComApenasUmaLetra() {
 		despesa.setLoja("L");
 		
@@ -32,6 +36,7 @@ class DespesaTest {
 	}
 	
 	@Test
+	@DisplayName("Setando Loja para String de várias letras")
 	void testSetLojaComVariasLetras() {
 		despesa.setLoja("Loja");
 		
@@ -39,11 +44,13 @@ class DespesaTest {
 	}
 
 	@Test
+	@DisplayName("Exception: campo produto vazio")
 	void testSetProdutoVazio() {
 		assertThrows(TextoNuloException.class, () -> despesa.setProduto(""));
 	}
 	
 	@Test
+	@DisplayName("Setando Produto para String de apenas uma letra")
 	void testSetProdutoComApenasUmaLetra() {
 		despesa.setProduto("P");
 		
@@ -51,6 +58,7 @@ class DespesaTest {
 	}
 	
 	@Test
+	@DisplayName("Setando Produto para String de várias letras")
 	void testSetProdutoComVariasLetras() {
 		despesa.setProduto("Produto");
 		
@@ -58,24 +66,28 @@ class DespesaTest {
 	}
 	
 	@Test
+	@DisplayName("Exception: ValorMensal igual a zero")
 	void testSetValorMensalVazio() {
 
 		assertThrows(ValorDespesaException.class, () -> despesa.setValorMensal(0));
 	}
 	
 	@Test
+	@DisplayName("Exception: ValorMensal é negativo")
 	void testSetValorMensalNegativo() {
 		assertThrows(ValorDespesaException.class, () -> despesa.setValorMensal(-2));
 	}
 	
 	@Test
+	@DisplayName("Setando ValorMensal para o valor mínimo de R$ 0,01")
 	void testSetValorMensalMinimo() {
-		despesa.setValorMensal(2.02);
+		despesa.setValorMensal(0.01);
 		
-		assertEquals(2.02, despesa.getValorMensal(), 1);
+		assertEquals(0.01, despesa.getValorMensal(), 1);
 	}
 	
 	@Test
+	@DisplayName("Setando ValorMensal para um valor baixo")
 	void testSetValorMensalBaixo() {
 		despesa.setValorMensal(21.12);
 		
@@ -83,6 +95,7 @@ class DespesaTest {
 	}
 	
 	@Test
+	@DisplayName("Setando ValorMensal para um valor alto")
 	void testSetValorMensalAlto() {
 		despesa.setValorMensal(2100.12);
 		
